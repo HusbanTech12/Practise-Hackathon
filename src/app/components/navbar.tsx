853514f3ae533810019f9876675382259a3ab191
HusbanTech12 <husbantech08@gmail.com>
2024-12-04 23:59:18 +0500

    initail commit

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,28 @@
+
+export default function Navbar(){
+    return (
+        <>
+            {/* Container */}
+            <div className="w-[1440px] h-[5902px] bg-[#FFFFFF]">
+                <header className="w-[1440px] h-[48px] bg-[#000000]">
+                    <div className="w-[859px] h-[24px] flex ">
+
+                    <p className="w-[550px] h-[24px] bg-slate-400">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <span>ShopNow</span></p>
+                    <p>English</p>
+                    </div>
+                </header>
+                
+                
+
+
+
+        </div>
+        
+        
+        
+        </>
+    )
+
+    
+    
+}
